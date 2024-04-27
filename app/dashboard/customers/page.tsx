a43938546c9a38d90c90072f199e8f6ab58f9478
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-04-27 19:46:26 -0300

    include create invoices action

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function Page() {
+export default async function Page() {
   return <div>Customer page</div>;
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,6 +36,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
+
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
