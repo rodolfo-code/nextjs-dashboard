0a94c9a8ce26a34e6bd8c2560d7ccd2536b6f7f4
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-05-04 12:19:55 -0300

    finalizado

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -166,7 +166,7 @@ export async function fetchInvoiceById(id: string) {
       WHERE invoices.id = ${id};
     `;
 
-    const invoice = data.rows.map((invoice) => ({
+    const invoice: any = data.rows.map((invoice) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
